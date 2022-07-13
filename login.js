e211ccb1f3c3ee264a165a189ad4836f3ef497a1
sofia <toastysocks02@gmail.com>
2022-07-13 10:32:18 +0800

    pls na

--- login.js
+++ login.js
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
 function validate(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     if(username == "webfund" && password == "admin"){
+        location.href = "homepage.html";
         alert("Login Successful");
         return true;
     }
@@ -10,17 +10,4 @@ function validate(){
         alert("Login Failed Please check username and password");
         return false;
     }
-=======
-function validate(){
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    if(username == "webfund" && password == "admin"){
-        alert("Login Successful");
-        return true;
-    }
-    else{
-        alert("Login Failed Please check username and password");
-        return false;
-    }
->>>>>>> e58de749f1ebd5097ce2b5fd33c5ae6ed85b6017
 }
