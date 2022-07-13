137c6fcc30d0becb94af5092b2dbe895750cac6d
sofia <toastysocks02@gmail.com>
2022-07-13 10:34:51 +0800

    aaaa

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ function validate(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     if(username == "webfund" && password == "admin"){
-        location.href = "homepage.html";
+        location.href = "homepage.html"
         alert("Login Successful");
         return true;
     }
